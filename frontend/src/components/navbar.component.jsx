a7475ab208030acfb62f455f550896ff75e5b5c0
Rajan <rajanj448@student.sfit.ac.in>
2024-02-11 11:56:09 +0530

    Navbar

--- frontend/src/components/navbar.component.jsx
+++ frontend/src/components/navbar.component.jsx
@@ -26,6 +26,16 @@ const Navbar = () => {
         >
           <i className="fi fi-rr-search text-xl"></i>
         </button>
+        <Link to="/editor" className="hidden md:flex gap-2 link">
+          <i className="fi fi-rr-file-edit"></i>
+          <p>Write</p>
+        </Link>
+        <Link className="btn-dark py-2 " to="/signin">
+          Sign In
+        </Link>
+        <Link className="btn-light py-2 hidden md:block " to="/signup">
+          Sign Up
+        </Link>
       </div>
     </nav>
   );
